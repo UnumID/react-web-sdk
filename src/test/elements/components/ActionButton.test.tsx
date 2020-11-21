79f09d7e662bd580cc7963069c57de5ac809a59c
gsmanian1970 <gs@sequoiaat.com>
2020-11-21 17:48:02 +0530

    Added the peerDependencies which was removed by oversight in the previous check in Also, added one more test case for ActionButton

--- src/test/elements/components/ActionButton.test.tsx
+++ src/test/elements/components/ActionButton.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import ActionButton from 'elements/components/ActionButton';
 
 describe('Button', () => {
   describe('render()', () => {
-    it('renders a primary button', () => {
-      const { container } = render(<ActionButton href="test" type="primary" onClick={jest.fn()}>I am a button</ActionButton>);
+    it('renders a primary action button', () => {
+      const { container } = render(<ActionButton href="test" type="primary">I am a button</ActionButton>);
       expect(container.firstChild).toMatchInlineSnapshot(`
         <a
           class="btn btn-primary"
@@ -15,6 +15,15 @@ describe('Button', () => {
           I am a button
         </a>
       `);
+    });
+  });
+
+  describe('actions', () => {
+    it('Checks the href attribute value', () => {
+      const { getByText } = render(<ActionButton href="test" type="primary">I am a button</ActionButton>);
+      const anchorBtn = getByText('I am a button');
+
+      expect(anchorBtn.href).toBe('http://localhost/test');
     });
   });
 });
