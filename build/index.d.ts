3f7de69c2272757b76d2020c86360c44d14bea6d
jacobleesinger <jacobleesinger@gmail.com>
2020-12-04 18:13:22 -0800

    export types

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,3 @@
 import WidgetHostAndController from 'components/WidgetHostAndController';
+export type { EmailOptions, SmsOptions, SuccessResponse, PresentationRequestResponse, } from 'types';
 export default WidgetHostAndController;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 import WidgetHostAndController from 'components/WidgetHostAndController';
+export type {
+  EmailOptions,
+  SmsOptions,
+  SuccessResponse,
+  PresentationRequestResponse,
+} from 'types';
 
 export default WidgetHostAndController;
