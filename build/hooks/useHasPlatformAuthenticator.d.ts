82231e8e545b91337f0dcd7f3b6960bb0bf6edd9
UnumID Admin <liam@unum.id>
2022-08-23 16:24:02 +0000

    [Release 1.6.1] Restricting supported platform authenticators to match web-wallet

--- build/hooks/useHasPlatformAuthenticator.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const detectHasPlatformAuthenticator: () => Promise<boolean>;
-export declare function useHasPlatformAuthenticator(): boolean;
-//# sourceMappingURL=useHasPlatformAuthenticator.d.ts.map
